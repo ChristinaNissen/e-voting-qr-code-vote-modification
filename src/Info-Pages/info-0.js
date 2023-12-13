1777116204404aa7b005277110302481881d2fdc
Christina Nissen <christinaf.nissen@hotmail.com>
2023-12-13 11:25:05 +0100

    Styling of consent form

--- src/Info-Pages/info-0.js
+++ src/Info-Pages/info-0.js
@@ -3,18 +3,18 @@
  *
  * This file is part of e-voting-system-self-replace.
  *
- * e-voting-system-self-replace is free software: you can redistribute it and/or modify
+ * e-voting-system-auto-replace is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * e-voting-system-self-replace is distributed in the hope that it will be useful,
+ * e-voting-system-auto-replace is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with e-voting-system-self-replace. If not, see <https://www.gnu.org/licenses/>.
+ * along with e-voting-system-auto-replace. If not, see <https://www.gnu.org/licenses/>.
  */
 
 import React from "react";
