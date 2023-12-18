8de250629f1a4c00face91f6991597b9874408ce
Christina Nissen <christinaf.nissen@hotmail.com>
2023-12-18 10:37:36 +0100

    Change layout to be correct on confirmation page

--- src/Voting-System/Confirmation/Confirmation.js
+++ src/Voting-System/Confirmation/Confirmation.js
@@ -1,20 +1,20 @@
 /**
  * Copyright 2023 Christina Frederikke Nissen, Elisabeth Fredrich
  *
- * This file is part of e-voting-system-auto-replace.
+ * This file is part of e-voting-system-self-replace.
  *
- * e-voting-system-auto-replace is free software: you can redistribute it and/or modify
+ * e-voting-system-self-replace is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * e-voting-system-auto-replace is distributed in the hope that it will be useful,
+ * e-voting-system-self-replace is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with e-voting-system-auto-replace. If not, see <https://www.gnu.org/licenses/>.
+ * along with e-voting-system-self-replace. If not, see <https://www.gnu.org/licenses/>.
  */
 
 import React from "react";
@@ -35,8 +35,8 @@ export default function Confirmation() {
             Once all votes have been counted, the results are published on our
             offical webpage.{" "}
           </Text>
-          <Text>
-            Please use this webpage to check, if your vote has been saved
+          <Text className="text-margin-top">
+            Please use this webpage to check, if your vote has been stored
             correctly by finding your unique verification code.
           </Text>
           <Button className="blue-btn" onClick={() => navigate("/info-2")}>
